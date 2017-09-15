c4b048d80f1b16f2afffa2651984b52b76063438
joefraley <joefraley@protonmail.com>
2017-09-15 10:36:41 -0700

    wip

--- main.js
+++ main.js
@@ -1,19 +1,7 @@
-const add = a => b => a + b;
-const double = num => num * 2;
-const square = num => num * num;
+const { createElement } = require("react");
+const { renderToString } = require("react-dom/server");
 
-const validateNumbers = a => {
-  const num = Number(a);
-  return num === NaN ? 0 : num;
-};
+const Text = ({ children, text }) =>
+  createElement("p", { children: text || children });
 
-const five = add(2)(3);
-const add5 = add(5);
-const nine = add5(4);
-
-const four = double(2);
-
-const isNum = validateNumbers(four);
-
-if (isNum) console.log("good job!");
-else console.log("nope...");
+renderToString(Text({ text: "hey" }));
