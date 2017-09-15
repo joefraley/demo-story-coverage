a235d2980fde47c0fa03d5eee01c1c27874fe76e
joefraley <joefraley@protonmail.com>
2017-09-15 10:40:07 -0700

    more

--- main.js
+++ main.js
@@ -3,5 +3,7 @@ const { renderToString } = require("react-dom/server");
 
 const Text = ({ children, text }) =>
   createElement("p", { children: text || children });
+const sayHey = () => Text({ text: "hey" });
+const render = dom => renderToString(dom());
 
-renderToString(Text({ text: "hey" }));
+render(sayHey);
