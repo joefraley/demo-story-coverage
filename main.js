9c5ac07e36e2856ab8403515a3a0f5db0c6c97da
joefraley <joefraley@protonmail.com>
2017-09-15 10:42:17 -0700

    stories

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
+const { storiesOf } = require("@storybook/react");
 const { createElement } = require("react");
 const { renderToString } = require("react-dom/server");
 
-const Text = ({ children, text }) =>
-  createElement("p", { children: text || children });
+const Text = ({ children, text }) => {
+  if (children) {
+    return createElement("p", { children: text });
+  } else {
+    return createElement("p", { children });
+  }
+};
+
 const sayHey = () => Text({ text: "hey" });
 const render = dom => renderToString(dom());
 
 render(sayHey);
+
+storiesOf("Text", module).add("can say hello", sayHey);
